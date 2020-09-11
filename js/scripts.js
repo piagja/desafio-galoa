3c3130ec1feb9b6f56d981fc8ede6b8cd855dd37
Gustavo Piá <piagja@gmail.com>
2020-09-11 20:29:43 -0300

    feat: resumo expandido

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/* configuração do resumo expandido */
+function resumoExpandido() { 
+  const tresPontos = document.querySelector('.ler-menos')
+  const maisTexto = document.querySelector('.ler-mais')
+  const toggleButton = document.querySelector('.toggle-ver-mais')
+
+  if (tresPontos.style.display === 'none') {
+    tresPontos.style.display = 'inline'
+    maisTexto.style.display = 'none'
+    toggleButton.style.fontWeight = 'bold'
+    toggleButton.style.color = '#ED7839'
+    toggleButton.innerHTML = 'ver mais'
+  } else {
+    tresPontos.style.display = 'none'
+    maisTexto.style.display = 'inline'
+    toggleButton.style.fontWeight = 'bold'
+    toggleButton.style.color = '#ED7839'
+    toggleButton.innerHTML = 'ver menos'
+  }
+}
