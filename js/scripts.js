84708dbfa803319e6c8f97e3da0726c0d05a9276
Gustavo Piá <piagja@gmail.com>
2020-09-14 18:04:04 -0300

    feat: criando topico expandido, feedback de topico e reload da pagina no botão

--- js/scripts.js
+++ js/scripts.js
@@ -19,13 +19,48 @@ function resumoExpandido () {
   }
 }
 
+/* configuração de resposta expandida */
+
 function respostaExpandida () {
   const cardExpandido = document.querySelector('.card-expandido')
 
-  console.log('a clicado')
   if (cardExpandido.style.display === 'none') {
     cardExpandido.style.display = 'inline'
   } else {
     cardExpandido.style.display = 'none'
   }
+}
+
+/* configuração de criar topico */
+
+function showForm () {
+  const criarTopicoForm = document.querySelector('.toggle-topico-discussoes')
+  const discussoes = document.querySelector('.criar-topico-discussoes')
+
+  if (criarTopicoForm.style.display !== 'none') {
+    discussoes.style.display = 'none'
+    criarTopicoForm.style.display = 'inline'
+  }
+}
+
+/* configuração de tópico enviado */
+
+function createdTopic () {
+  const toggle = document.querySelector('.toggle-topico-discussoes')
+  const feedback = document.querySelector('.topico-criado-wrapper')
+
+  toggle.addEventListener('click', (event) => {
+    event.preventDefault()
+    if (toggle.style.display === 'inline') {
+      toggle.style.display = 'none'
+      feedback.style.display = 'inline'
+      return false
+    }
+  })
+}
+
+/* configurar o botão de "criar novo tópico" para apenas dar refresh na página */
+
+function reload () {
+  window.location.reload()
 }
